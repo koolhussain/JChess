a13760b436410f796560a91898fc853db9c552ac
Ahmad Hussain <ahmad.hussain91@gmail.com>
2018-09-29 15:54:27 +0530

    1st Commit

---
+++ JChess/src/com/chess/engine/board/Tile.java
@@ -0,0 +1,80 @@
+package com.chess.engine.board;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.chess.engine.pieces.Piece;
+import com.google.common.collect.ImmutableMap;
+
+//Class is Immutable
+public abstract class Tile {
+	
+	protected final int tileCoordinate;
+	
+	private static final Map<Integer, EmptyTile>EMPTY_TILES = createAllPossibleEmptyTiles();
+	
+	private Tile(int tileCoordinate) {
+		this.tileCoordinate = tileCoordinate;
+	}
+	
+	private static Map<Integer, EmptyTile> createAllPossibleEmptyTiles() {
+		
+		final Map<Integer, EmptyTile> emptyTileMap = new HashMap<>();
+		
+		for(int i =0; i < 64; i++) {
+			emptyTileMap.put(i, new EmptyTile(i));
+		}
+		
+		return ImmutableMap.copyOf(emptyTileMap);
+	}
+	
+	public static Tile createTile(final int tileCoordinate, final Piece piece) {
+		return piece != null ? new OccupiedTile(tileCoordinate, piece) : EMPTY_TILES.get(tileCoordinate);
+	}
+
+	//Abstract Method returns whether there is a Piece on a Tile
+	public abstract boolean isTileOccupied();
+	
+	//Abstract Method returns the Piece Object that is on a Tile
+	public abstract Piece getPiece();
+	
+	//Tile with no piece on it.
+	public static final class EmptyTile extends Tile {
+		
+		EmptyTile(final int coordinate) {
+			super(coordinate);
+		}
+		
+		@Override
+		public boolean isTileOccupied() {
+			return false;
+		}
+		
+		@Override
+		public Piece getPiece() {
+			return null;
+		}
+	}
+	
+	//Tile with a Piece on it.
+	public static final class OccupiedTile extends Tile {
+		
+		private final Piece pieceOnTile;
+		
+		OccupiedTile(int tileCoordinate, Piece pieceOnTile) {
+			super(tileCoordinate);
+			this.pieceOnTile = pieceOnTile;
+		}
+		
+		@Override
+		public boolean isTileOccupied() {
+			return true;
+		}
+		
+		@Override
+		public Piece getPiece() {
+			return this.pieceOnTile;
+		}
+	}
+
+}
